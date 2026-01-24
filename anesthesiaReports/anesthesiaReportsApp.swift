f7652d9873209b1edfdfa2a43fdb830d4c6efc5d
Renan Dantas <renandw@me.com>
2026-01-24 17:25:42 -0400

    Initial Commit

---
+++ anesthesiaReports/anesthesiaReportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  anesthesiaReportsApp.swift
+//  anesthesiaReports
+//
+//  Created by Renan Wrobel on 24/01/26.
+//
+
+import SwiftUI
+
+@main
+struct anesthesiaReportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ anesthesiaReportsTests/anesthesiaReportsTests.swift
@@ -0,0 +1,17 @@
+//
+//  anesthesiaReportsTests.swift
+//  anesthesiaReportsTests
+//
+//  Created by Renan Wrobel on 24/01/26.
+//
+
+import Testing
+@testable import anesthesiaReports
+
+struct anesthesiaReportsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
